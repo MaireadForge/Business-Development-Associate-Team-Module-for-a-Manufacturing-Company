b61a34856db0669961c1ba7edf8ef1f3a8312e2c
MaireadForge <169300900+MaireadForge@users.noreply.github.com>
2026-05-24 23:31:37 +0530

    Connecetd frontend to deployed backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://business-development-associate-team-i0jp.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
